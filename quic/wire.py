c311448796ae1db93ddac22cd5d85d56e4b1ccd7
Alexander Zhukov <zhukovaa90@gmail.com>
2017-06-12 18:49:25 +0300

    Use buffer

--- quic/wire.py
+++ quic/wire.py
@@ -11,18 +11,18 @@ class Frame:
             setattr(self, name, val)
 
     def __eq__(self, other):
-        print(self.__dict__, other.__dict__)
+        # print(self.__dict__, other.__dict__)
         return self.__dict__ == other.__dict__
 
     @classmethod
-    def from_bytes(cls, bytedata):
+    def from_bytes(cls, buffer):
 
-        type_byte = bytedata[0]
+        type_byte = buffer.read1(1)[0]
 
         if type_byte >= 192:
-            return StreamFrame.from_bytes(bytedata)
+            return StreamFrame.from_bytes(buffer)
         if type_byte >= 160:
-            return AckFrame.from_bytes(bytedata)
+            return AckFrame.from_bytes(buffer)
 
         switch = {
             PaddingFrame.TYPE_BYTE[0]: PaddingFrame,
@@ -40,7 +40,7 @@ class Frame:
         }
 
         try:
-            return switch[type_byte].from_bytes(bytedata)
+            return switch[type_byte].from_bytes(buffer)
         except KeyError:
             raise Exception('Invalid frame type')
 
@@ -54,7 +54,7 @@ class RegularFrame(Frame):
         return self.TYPE_BYTE
 
     @classmethod
-    def from_byte(cls, bytedata):
+    def from_byte(cls, buffer):
         return cls()
 
 
@@ -71,10 +71,10 @@ class ResetStreamFrame(RegularFrame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder),
-                   int.from_bytes(bytedata[5:12], sys.byteorder),
-                   int.from_bytes(bytedata[13:16], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder),
+                   int.from_bytes(buffer.read1(8), sys.byteorder),
+                   int.from_bytes(buffer.read1(4), sys.byteorder))
 
     def to_bytes(self):
         return (self.TYPE_BYTE +
@@ -91,9 +91,10 @@ class ConnectionCloseFrame(RegularFrame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder),
-                   bytedata[7:7 + int.from_bytes(bytedata[5:7], sys.byteorder)])
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder),
+                   buffer.read1(
+                    int.from_bytes(buffer.read1(2), sys.byteorder)))
 
     def to_bytes(self):
         return (self.TYPE_BYTE +
@@ -110,9 +111,9 @@ class GoAwayFrame(RegularFrame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder),
-                   int.from_bytes(bytedata[5:8], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder),
+                   int.from_bytes(buffer.read1(4), sys.byteorder))
 
     def to_bytes(self):
         return (self.TYPE_BYTE +
@@ -128,8 +129,8 @@ class MaxDataFrame(Frame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder))
 
     def to_bytes(self):
         return self.TYPE_BYTE + self.max_data.to_bytes(4, sys.byteorder)
@@ -144,9 +145,9 @@ class MaxStreamDataFrame(Frame):
         self.offset = offset
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder),
-                   int.from_bytes(bytedata[5:12], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder),
+                   int.from_bytes(buffer.read1(8), sys.byteorder))
 
     def to_bytes(self):
         return (self.TYPE_BYTE +
@@ -162,8 +163,8 @@ class MaxStreamIDFrame(Frame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder))
 
     def to_bytes(self):
         return self.TYPE_BYTE + self.max_stream_id.to_bytes(4, sys.byteorder)
@@ -182,8 +183,8 @@ class BlockedFrame(Frame):
         self.stream_id = stream_id
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder))
 
     def to_bytes(self):
         return self.TYPE_BYTE + self.stream_id.to_bytes(4, sys.byteorder)
@@ -197,8 +198,8 @@ class StreamBlockedFrame(Frame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:4], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(4), sys.byteorder))
 
     def to_bytes(self):
         return self.TYPE_BYTE + self.stream_id.to_bytes(4, sys.byteorder)
@@ -217,22 +218,14 @@ class NewConnectionIDFrame(Frame):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        return cls(int.from_bytes(bytedata[1:2], sys.byteorder),
-                   int.from_bytes(bytedata[3:6], sys.byteorder))
+    def from_bytes(cls, buffer):
+        return cls(int.from_bytes(buffer.read1(2), sys.byteorder),
+                   int.from_bytes(buffer.read1(8), sys.byteorder))
 
     def to_bytes(self):
         return (self.TYPE_BYTE +
                 self.sequence.to_bytes(2, sys.byteorder) +
                 self.connection_id.to_bytes(8, sys.byteorder))
-
-
-class StopWaitingFrame(Frame):
-    """"""
-
-
-
-
 
 
 class StreamFrame(Frame):
@@ -244,28 +237,24 @@ class StreamFrame(Frame):
         self.payload = payload
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        type_byte = bytedata[0]
-        fin = type_byte & 0b00100000 > 0
-        data_len_present = type_byte & 0b00010000 > 0
-        offset_len = type_byte & 0x1C >> 2
-        pos = 1
+    def from_bytes(cls, buffer):
+        buffer.seek(-1, 1)
+        type_byte = buffer.read1(1)[0]
+        fin = type_byte & 0x20  > 0
+        data_len_present = type_byte & 0x10 > 0
+        offset_len = type_byte & 0x0C >> 2
 
-        stream_id_len = (type_byte & 0x03) + 1
+        stream_id_len = (type_byte & 0x03)
 
-        stream_id = int.from_bytes(bytedata[1:stream_id_len + 1], sys.byteorder)
-        pos += stream_id_len
-        offset = int.from_bytes(bytedata[pos:pos + offset_len + 1], sys.byteorder)
-        pos += offset_len
+        stream_id = int.from_bytes(buffer.read1(stream_id_len), sys.byteorder)
+        offset = int.from_bytes(buffer.read1(offset_len), sys.byteorder)
 
         if not data_len_present:
-            data_length = len(bytedata) - pos
+            data_length = len(bytedata)
         else:
-            data_length = int.from_bytes(bytedata[pos:pos + 3], sys.byteorder)
-            pos += 3
+            data_length = int.from_bytes(buffer.read1(2), sys.byteorder)
 
-        payload = bytedata[pos:data_length]
-        return cls(stream_id, offset, fin, payload)
+        return cls(stream_id, offset, fin, buffer.read1(data_length))
 
     def _get_sream_id_length(self):
         return self.stream_id.bit_length() // 8 + 1
@@ -286,10 +275,10 @@ class StreamFrame(Frame):
         offset_length = self._get_offset_length()
 
         if offset_length > 0:
-            type_byte ^= (offset_length - 1) << 2
+            type_byte ^= offset_length << 2
 
         stream_id_len = self._get_sream_id_length()
-        type_byte ^= stream_id_len - 1
+        type_byte ^= stream_id_len
 
         stream_id = self.stream_id.to_bytes(stream_id_len, sys.byteorder)
         offset = self.offset.to_bytes(offset_length, sys.byteorder)
@@ -298,53 +287,45 @@ class StreamFrame(Frame):
         return type_byte.to_bytes(1, sys.byteorder) + stream_id + offset + data_len + self.payload
 
 
-
 class AckFrame(Frame):
     """"""
     
-
     def __init__(self, largest_acknowledged, ack_delay, ack_blocks, timestapms):
         super().__init__(locals())
 
     @classmethod
-    def from_bytes(cls, bytedata):
-        type_byte = bytedata[0]
-        n = type_byte & 0x10
+    def from_bytes(cls, buffer):
+        buffer.seek(-1, 1)
+        type_byte = buffer.read1(1)[0]
         ll = type_byte & 0x0c
         mm = type_byte & 0x03
         pos = 1
         if type_byte & 0x10:
-            num_blocks = bytedata[1]
+            num_blocks = buffer.read1(1)[0]
             pos = 2
         else:
             num_blocks = 1
 
-        num_ts = bytedata[pos]
-        pos += 1
+        num_ts = buffer.read1(1)[0]
         largest_acknowledged_len = (1, 2, 4, 6)[ll >> 2]
-        largest_acknowledged = int.from_bytes(bytedata[pos:pos + largest_acknowledged_len], sys.byteorder)
-        pos += largest_acknowledged_len
-        ack_delay = int.from_bytes(bytedata[pos:pos + 2], sys.byteorder)
-        pos += 2
+        largest_acknowledged = int.from_bytes(buffer.read1(largest_acknowledged_len), sys.byteorder)
+        ack_delay = int.from_bytes(buffer.read1(2), sys.byteorder)
         ack_blocks = []
-        ack_blocks.append(int.from_bytes(bytedata[pos:pos + largest_acknowledged_len], sys.byteorder))
-        pos += largest_acknowledged_len
+        ack_blocks.append(int.from_bytes(buffer.read1(largest_acknowledged_len), sys.byteorder))
         for i in range(1, num_blocks):
             ack_blocks.append(
-                (int.from_bytes(bytedata[pos:pos + 1], sys.byteorder),
-                 int.from_bytes(bytedata[pos + 1:pos + 1 + largest_acknowledged_len], sys.byteorder)),
+                (int.from_bytes(buffer.read1(1), sys.byteorder),
+                 int.from_bytes(buffer.read1(largest_acknowledged_len), sys.byteorder)),
             )
-            pos += 1 + largest_acknowledged_len
 
         timestapms = []
         if num_ts:
             timestapms.append(
-                (bytedata[pos], int.from_bytes(bytedata[pos + 1: pos + 4], sys.byteorder))
+                (buffer.read1(1)[0], int.from_bytes(buffer.read1(4), sys.byteorder))
             )
-            pos += 1 + 4
         for i in range(1, num_ts):
             timestapms.append(
-                (bytedata[pos], read_ufloat16(bytedata[pos + 1:pos + 3]))
+                (buffer.read1(1)[0], read_ufloat16(buffer.read1(2)))
             )
             pos += 3
 
@@ -395,32 +376,33 @@ class QUICPacket:
 
 
 if __name__ == '__main__':
-    f = StreamFrame(1, 3, False, b'test')
-    assert Frame.from_bytes(f.to_bytes()) == f
+    import io
+    f = StreamFrame(32, 3, False, b'test')
+    assert Frame.from_bytes(io.BytesIO(f.to_bytes())) == f
 
     rst_stream = ResetStreamFrame(1, 1, 1)
-    assert Frame.from_bytes(rst_stream.to_bytes()) == rst_stream
+    assert Frame.from_bytes(io.BytesIO(rst_stream.to_bytes())) == rst_stream
 
     blocked = BlockedFrame(1)
-    assert Frame.from_bytes(blocked.to_bytes()) == blocked
+    assert Frame.from_bytes(io.BytesIO(blocked.to_bytes())) == blocked
 
     stream_blocked = StreamBlockedFrame(1)
-    assert Frame.from_bytes(stream_blocked.to_bytes()) == stream_blocked
+    assert Frame.from_bytes(io.BytesIO(stream_blocked.to_bytes())) == stream_blocked
 
     max_data = MaxDataFrame(1)
-    assert Frame.from_bytes(max_data.to_bytes()) == max_data
+    assert Frame.from_bytes(io.BytesIO(max_data.to_bytes())) == max_data
 
     new_conn_id = NewConnectionIDFrame(1, 1)
-    assert Frame.from_bytes(new_conn_id.to_bytes()) == new_conn_id
+    assert Frame.from_bytes(io.BytesIO(new_conn_id.to_bytes())) == new_conn_id
 
     max_stream_id = MaxStreamIDFrame(1)
-    assert Frame.from_bytes(max_stream_id.to_bytes()) == max_stream_id
+    assert Frame.from_bytes(io.BytesIO(max_stream_id.to_bytes())) == max_stream_id
 
     conn_close = ConnectionCloseFrame(1, b'game over')
-    assert Frame.from_bytes(conn_close.to_bytes()) == conn_close
+    assert Frame.from_bytes(io.BytesIO(conn_close.to_bytes())) == conn_close
 
     goaway = GoAwayFrame(1, 1)
-    assert Frame.from_bytes(goaway.to_bytes()) == goaway
+    assert Frame.from_bytes(io.BytesIO(goaway.to_bytes())) == goaway
 
     ack = AckFrame(1, 2, [3, (4, 5)], [(1, 2), (3, 4), (5, 6)])
-    assert Frame.from_bytes(ack.to_bytes()) == ack
+    assert Frame.from_bytes(io.BytesIO(ack.to_bytes())) == ack
